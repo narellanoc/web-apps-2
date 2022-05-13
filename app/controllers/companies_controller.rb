10e4a4218c2dacdf5721aeb7dcd08d31caaad7c1
narellanoc <nicolas.arellano@kellogg.northwestern.edu>
2022-05-13 21:38:44 +0000

    clase

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,9 +3,26 @@ class CompaniesController < ApplicationController
   # TODO:
   # def index
   # end
+  def index
+    @companies = Company.all
+  end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+    @contacts = Contact.where({"company_id" => @company["id"] })
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["state"] = params["company"]["state"]
+    @company["city"] = params["company"]["city"]
+    @company.save
+    redirect_to "/companies"
   end
 
 end
